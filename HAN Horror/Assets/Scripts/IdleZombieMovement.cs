a52ceb2667d6a3aab6ce4ad9bb32fae8caa94ecc
SWLdeBruin <sjoerddebruin1@hotmail.com>
2023-10-03 16:03:12 +0200

    thing

--- HAN Horror/Assets/Scripts/IdleZombieMovement.cs
+++ HAN Horror/Assets/Scripts/IdleZombieMovement.cs
@@ -1,18 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class IdleZombieMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private NavMeshAgent agent;
+    public Transform goal;
+
+    private GameObject player;
+    private float aggroDistance = 10;
+
+    private Animator animator;
+
+    enum States
     {
-        
+        Idle,
+        Aggro,
+        Scared
+    }
+    private States currentState;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player (1)");
+
+        currentState = States.Idle;
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = transform.position;
+
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        // Physics.OverlapSphere();
+        switch (currentState)
+        {
+            case States.Idle:
+                // Transition to Aggro
+                if ((transform.position - player.transform.position).magnitude <= aggroDistance)
+                {
+                    currentState = States.Aggro;
+                    animator.SetBool("Run", true);
+                }
+                break;
+
+            case States.Aggro:
+                agent.destination = player.transform.position;
+
+                // Transition to Patrol
+                if ((transform.position - player.transform.position).magnitude > aggroDistance)
+                {
+                    currentState = States.Idle;
+                    animator.SetBool("Run", false);
+                }
+
+                if ((transform.position - player.transform.position).magnitude < 1.5)
+                {
+                    animator.SetTrigger("Punch");
+                    SceneManager.LoadScene("DeathScreen");
+                }
+                break;
+        }
     }
 }
